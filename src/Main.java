108954da57a5d06d9af88a09b7beb8c6ad59f1ff
Nekrasov Aleksandr <a.nekrasov@qa-academy.by>
2021-11-17 22:27:00 +0300

    НАброски для проверки

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -12,7 +14,24 @@ public class Main {
 //        System.out.println(NumberConvector.arabicToRoman(Calculator
 //                                                        .RPNtoAnswer(Calculator
 //                                                        .ExpressionToRPN(expression))));
-        System.out.println(Calculator.RPNtoAnswer(Calculator.ExpressionToRPN(expression)));
+//        System.out.println(Calculator.RPNtoAnswer(Calculator.ExpressionToRPN(expression)));
+        char sim = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = 0; i<expression.length(); i++) {
+            sim = expression.charAt(i);
+            if(getPriority(sim)==0){
+                stringBuilder.append(sim);
+            }
+        }
+        System.out.print(NumberConvector.romanToArabic(stringBuilder.toString()));
 
+
+    }
+
+    private static int getPriority(char token){
+        if(token == '*' || token == '/' ) return 3;
+        else if(token == '+' || token == '-') return 2;
+        else if(token == ' ') return 1;
+        else return 0;
     }
 }
