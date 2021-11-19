2e7b1f5739261508157c7a2bf4b9379d7f03d899
Nekrasov Aleksandr <a.nekrasov@qa-academy.by>
2021-11-19 18:08:16 +0300

    Addeв changes to main method

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,23 @@
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String expression = scanner.nextLine();
 
-
+        String[] line = expression.split(" ");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i=0; i < line.length; i++) {
+                if(!line[i].contains("+")&&!line[i].contains("-")&&!line[i].contains("*")&&!line[i].contains("/"))
+                    stringBuilder.append(line[i]);
+        }
+        if (Integer.parseInt(stringBuilder.toString())<=1010) {
+            System.out.println(Calculator.RPNtoAnswer(Calculator.ExpressionToRPN(expression)));
+        } else throw new InputMismatchException("The number is not on the interval 1 - 10");
 
             // Результат чисел
-            System.out.println(Calculator.RPNtoAnswer(Calculator.ExpressionToRPN(expression)));
+            // System.out.println(Calculator.RPNtoAnswer(Calculator.ExpressionToRPN(expression)));
 
 
 
@@ -20,4 +25,5 @@ public class Main {
         //System.out.println(Convector.arabicToRoman(Calculator
          //       .RPNtoAnswer(Calculator.ExpressionToRPN(RomaToArabic.arabicExpression(expression)))));
     }
+
 }
